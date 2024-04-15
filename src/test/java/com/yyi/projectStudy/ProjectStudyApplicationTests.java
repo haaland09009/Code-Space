3545e73080f5cd8925a1619f24edd506b59e5836
haaland <dbsduddls09@naver.com>
2024-04-15 20:11:46 +0900

    테스트 어노테이션 삭제

--- src/test/java/com/yyi/projectStudy/ProjectStudyApplicationTests.java
+++ src/test/java/com/yyi/projectStudy/ProjectStudyApplicationTests.java
@@ -3,11 +3,8 @@ package com.yyi.projectStudy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ProjectStudyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
--- src/test/java/com/yyi/projectStudy/repository/ProjectRepositoryTests.java
+++ src/test/java/com/yyi/projectStudy/repository/ProjectRepositoryTests.java
@@ -3,9 +3,7 @@ package com.yyi.projectStudy.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 public class ProjectRepositoryTests {
 
-    @Autowired
-    private ProjectRepository projectRepository;
 }
